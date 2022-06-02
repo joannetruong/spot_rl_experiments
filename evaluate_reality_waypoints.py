e0da33942a940effbb74a7fb0cecf3c096415c29
Lab <lab@Labs-MacBook-Pro.local>
2022-06-01 17:04:08 -0700

    fix resetting and yaw for waypoint nav

--- evaluate_reality_waypoints.py
+++ evaluate_reality_waypoints.py
@@ -10,7 +10,7 @@ import torch.cuda
 from nav_env import SpotNavEnv
 from omegaconf import OmegaConf
 from real_policy import NavPolicy
-from spot_wrapper.spot import Spot
+from spot_wrapper.spot import Spot, wrap_heading
 
 @hydra.main(config_path="config", config_name="waypoint_config")
 def main(cfg):
@@ -25,11 +25,29 @@ def main(cfg):
     env = SpotNavEnv(spot, cfg)
     env.sensor_type = cfg.sensor_type
     waypoints = cfg.waypoints
-    for waypoint in waypoints:
-        goal_x, goal_y = waypoint.goal_x, waypoint.goal_y
+
+    for idx in waypoints:
+        policy.reset()
+        goal_x, goal_y = waypoints[idx].goal_x, waypoints[idx].goal_y
         print(f"NAVIGATING TO X: {goal_x}m, Y: {goal_y}m")
-        print("curr pose: ", spot.get_xy_yaw())
-        observations = env.reset([goal_x, goal_y])
+        cx, cy, cyaw = spot.get_xy_yaw()
+        print("curr pose: ", cx, cy, np.rad2deg(cyaw))
+        if idx == 0:
+            observations = env.reset([goal_x, goal_y])
+            home_x, home_y, home_t = spot.get_xy_yaw()
+            print('HOME POSE: ', home_x, home_y, home_t)
+        else:
+            if cfg.use_local_coords:
+                cx, cy, cyaw = spot.get_xy_yaw()
+                spot.home_robot(yaw=spot.boot_yaw)
+                cx, cy, cyaw = spot.get_xy_yaw()
+                print("homed curr pose: ", cx, cy, np.rad2deg(cyaw))
+            env.goal_xy = np.array([goal_x, goal_y], dtype=np.float32)
+            env.num_actions = 0
+            env.num_collisions = 0
+            env.episode_distance = 0
+            env.num_steps = 0
+        print('STEPPING NOW!')
         done = False
         stop_time = None
         if cfg.timeout != -1:
@@ -61,8 +79,11 @@ def main(cfg):
                     )
                     print("Final # Actions: {}".format(env.num_actions))
                     print("Final # Collisions: {}".format(env.num_collisions))
-        finally:
-            spot.power_off()
+        except:
+            pass
+        input('press to continue')
+    time.sleep(20)
+    spot.power_off()
 
 
 if __name__ == "__main__":
